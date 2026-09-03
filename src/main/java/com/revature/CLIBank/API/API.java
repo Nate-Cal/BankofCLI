6d4fb5f578a60308807283525583edadc151c9b7
NateCal715 <157073389+NateCal715@users.noreply.github.com>
2026-09-03 14:03:41 -0500

    Added signUp and returningUser in API.java. Updated login in API.java

--- src/main/java/com/revature/CLIBank/API/API.java
+++ src/main/java/com/revature/CLIBank/API/API.java
@@ -6,7 +6,9 @@ public class API {
     public static void main(String[] args) {
         System.out.println("Welcome to the Bank of CLI!");
         System.out.println();
-        login();
+        returningUser();
+
+
 
         System.out.println();
 
@@ -14,6 +16,34 @@ public class API {
 
     }
 
+    public static void returningUser() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Are you a returning User? (y/n): ");
+            String yesNo = scanner.nextLine();
+            if (yesNo.equals("y")) {
+                System.out.println();
+                login();
+                return;
+            } if (yesNo.equals("n")) {
+                System.out.println();
+                signUp();
+            } else {
+                System.out.println("Invalid input");
+                returningUser();
+            }
+        }
+    }
+    public static void signUp() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Welcome New User! Please create an account...");
+            System.out.print("Create a Username: ");
+            String name = scanner.nextLine();
+            System.out.println("Please create a PIN: ");
+            System.out.println("PIN must include ...");
+            String pinAsString = scanner.nextLine();
+            int pin = Integer.parseInt(pinAsString);
+        }
+    }
 
     public static void login() {
         try (Scanner scanner = new Scanner(System.in)) {
@@ -31,6 +61,10 @@ public class API {
     }
 
     public static void withdraw() {
+
+    }
+
+    public static void transfer() {
 
     }
 
