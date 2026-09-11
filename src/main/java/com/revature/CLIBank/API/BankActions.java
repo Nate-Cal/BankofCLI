9883f82c4c6d3b3b45e9f53b9be07698022517f9
mosaha <madhurasaha20@gmail.com>
2026-09-11 00:57:53 -0400

    initial deposit and withdrawal added

---
+++ src/main/java/com/revature/CLIBank/API/BankActions.java
@@ -0,0 +1,28 @@
+package com.revature.CLIBank.API;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class BankActions {
+
+    //if there is a maximum deposit amount, that check will go here
+    //AccountInfo needs to a new constructor that accepts the AccountID parameter
+    /*public boolean checkWithdrawal(UUID accountID, BigDecimal amount){
+        // getBalance method needs to be added to the AccountInfo class
+        AccountInfo accountInfo = new AccountInfo(accountID);
+        BigDecimal balance;
+        balance = accountInfo.getBalance(accountID);
+        if(balance.compareTo(amount) >= 0)
+        {
+            return true;
+        } else
+            return false;
+        {
+        }
+    } */
+
+   /* public boolean checkTransfer(UUID accountIDone, UUID accountIDtwo, BigDecimal amount ){
+        checkWithdrawal(accountIDone, amount)
+    }
+    */
+}
