35e7a382539f9674660050416a0e7a92d2d87562
Juan Vasquez <juandavidvasquezescobar2020@gmail.com>
2026-09-15 15:31:34 -0400

    Initializing the database in API.java and adding some functionality for the database to work

--- src/main/java/com/revature/CLIBank/API/API.java
+++ src/main/java/com/revature/CLIBank/API/API.java
@@ -17,6 +17,7 @@ public class API {
         this.user = null;
         this.result = null;
         this.bankTransactions = new BankTransactions();
+        new AccountRepo().initSchema();
     }
 
     /**
--- src/main/java/com/revature/CLIBank/Utility/ConnectionFactory.java
+++ src/main/java/com/revature/CLIBank/Utility/ConnectionFactory.java
@@ -1,6 +1,5 @@
 package com.revature.CLIBank.Utility;
 
-// import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,8 +7,20 @@ import java.sql.Statement;
 
 public class ConnectionFactory {
 
-    private static final String url = System.getenv("DATABASE-PATH");
+    private static final String DEFAULT_URL = "jdbc:sqlite:./data/bank.db";
+    private static final String url = finalUrl();
 
+    /** 
+     * Method to check if there is an environment variable for the database
+     * If not, reads the database in data/bank.db
+     */
+    private static String finalUrl() {
+        String fromEnv = System.getenv("DATABASE-PATH");
+        if (fromEnv == null || fromEnv.isBlank()) {
+            return DEFAULT_URL;
+        }
+        return fromEnv;
+    }
 
     public static Connection getAutoCommitConnect() throws SQLException {
         Connection connection = DriverManager.getConnection(url);
